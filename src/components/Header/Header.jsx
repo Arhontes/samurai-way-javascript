6c9c39828d6dfe8c97f2172edcb823514d18d9bb
Arhontes <arhontes@yandex.ru>
2022-03-26 12:08:45 +0300

    Changed Header.jsx and Header.module.css. Add import to module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import './Header.module.css'
+import s from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
-      <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"></img>
+    <header className={s.header}>
+      <img className={s.img} src = "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"></img>
     </header>
   );
 };
