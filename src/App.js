7c03d5c1ba7e3c9378944bd99f00c9e6ea414e5f
Arhontes <arhontes@yandex.ru>
2022-03-27 11:16:33 +0300

    add Route path to App.js end of lesson ReactJs 19 (route, browser-router)

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <NavBar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
-                {/*<Profile/>*/}
-            </div>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <NavBar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path="/messages" element={<Dialogs/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                    </Routes>
+                   {/*<Profile/>*/}
+                </div>
 
-        </div>
+            </div>
+        </BrowserRouter>
+
     );
 };
 
