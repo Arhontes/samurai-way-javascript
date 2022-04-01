16e2a22d8950b9849cba5152962fde57cbbef64f
Arhontes <arhontes@yandex.ru>
2022-04-01 09:45:42 +0300

    Changed Messages page: added dialog as dialog item, added message fields using grid; configured css module

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,30 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog+ ' ' + s.active}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                Sasha
+                </div>
+                <div className={s.dialog}>
+                Sasha
+                </div>
+
+            </div>
+            <div className={s.messages}>
+
+            <div className="message"> Hi</div>
+            <div className="message">How are you</div>
+            <div className="message">I'm fine, thanks</div>
         </div>
+        </div>
+
     )
 }
 export default Dialogs;
