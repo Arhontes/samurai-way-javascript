5c89900b66c0c5b3715196caa42237b27eabdef9
Arhontes <arhontes@yandex.ru>
2022-04-06 18:25:08 +0300

    added function addPost. Now it invoke alert message

--- src/components/Profile/Myposts/MyPosts.jsx
+++ src/components/Profile/Myposts/MyPosts.jsx
@@ -2,12 +2,17 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 import Message from "../../Dialogs/Message/Message";
 
+function addPost(){alert('YOO!')}
 
 const MyPosts = (props) => {
 
     let postsElement = props.posts.map(post=><Post message={post.message} /> )
   return (
     <div >
+        <div>
+            <button onClick={addPost}>Add post
+            </button>
+        </div>
 
         {postsElement}
 
