8e380c028a53e79d40a5e6e1ec50eb5235f96120
Arhontes <arhontes@yandex.ru>
2022-04-14 08:21:39 +0300

    Modified css in Dialogs.module.css. added new flex attribute

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,27 +2,29 @@ import s from './Dialogs.module.css'
 import Message from "./Message/Message";
 import DialogItem from "./DialogItem/DialogItem";
 import React from "react";
+
 let newMessageElement = React.createRef()
 
-function sendMessage() {
+let sendMessage = () => {
 
     let textMessage = newMessageElement.current.value
     alert(textMessage)
 }
 const Dialogs = (props) => {
 
-    let dialogsElement = props.data.dialogsPage.dialogs.map(d=> <DialogItem name={d.name} id={d.id} />);
-    let messagesElement = props.data.dialogsPage.messages.map(message=><Message message={message.message} />);
-    return(
+    let dialogsElement = props.data.dialogsPage.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElement = props.data.dialogsPage.messages.map(message => <Message message={message.message}/>);
+    return (
         <div className={s.dialogs}>
 
-            <div >
+            <div>
                 {dialogsElement}
             </div>
             <div className={s.messages}>
-                <div>
-
-
+                <div className={s.listOfMessage}>
+                    {messagesElement}
+                </div>
+                <div className={s.inputArea}>
                     <div className={s.buttons}>
                         <textarea ref={newMessageElement}></textarea>
                         <button onClick={sendMessage}>
@@ -31,7 +33,6 @@ const Dialogs = (props) => {
                     </div>
 
                 </div>
-                {messagesElement}
             </div>
         </div>
     )
