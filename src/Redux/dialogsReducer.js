ec43354e76faf0f23f64e002fe2d5a2082917b33
Arhontes <arhontes@yandex.ru>
2022-04-14 19:35:50 +0300

    Began to use redux this lesson. Added new file reduxStore.js. applied reducers for redux.store

--- src/Redux/dialogsReducer.js
+++ src/Redux/dialogsReducer.js
@@ -1,7 +1,25 @@
 const add_message = 'ADD-MESSAGE';
 const update_newMessageText = 'UPDATE-NEW-MESSAGE-TEXT';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dataOfCurrentMessage: {id: 0, message: 'add text here'},
+    messages: [
+        {id: 1, message: 'Good morning!'},
+        {id: 2, message: 'Good afternoon!'},
+        {id: 3, message: 'Good evening!'},
+        {id: 4, message: 'Good night!'},
+        {id: 5, message: 'Good day!'}
+    ],
+    dialogs: [
+        {id: 1, name: 'Sasha'},
+        {id: 2, name: 'Leha'},
+        {id: 3, name: 'Michael'},
+        {id: 4, name: 'Dobrinya'},
+        {id: 5, name: 'Max'}
+    ]
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case update_newMessageText:
             state.dataOfCurrentMessage = action.newMessage
--- src/Redux/profileReducer.js
+++ src/Redux/profileReducer.js
@@ -1,7 +1,15 @@
 const update_newPostText = 'UPDATE-NEW-POST-TEXT';
 const add_post = 'ADD-POST';
 
-const profileReducer = (state, action)=>{
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?'},
+        {id: 2, message: 'I\'m fine,thanks'}
+    ],
+    newPostText: "Zdarova Sanya!"
+}
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case add_post:
             let newPost = {id: 5, message: state.newPostText};
